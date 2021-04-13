8d63c4bc5807b6a92736cc817348b74fcccf59f8
Blacklist14 <luis.arigato69@hotmail.com>
2021-04-13 11:43:41 -0300

    update 2.0

--- javascript/Menu.js
+++ javascript/Menu.js
@@ -1,12 +1,20 @@
 i = 0
 var menu = document.querySelector(".hamburgue").addEventListener("click", () => {
     var move = document.querySelector(".menu-content")
+    var somb = document.querySelector("#sombra")
+    var hid = document.querySelector(".content-video")
     if (i <= 0) {
-        move.style.transform = ("translateX(-300px)")
+        hid.style.overflow ="hidden"
+        move.style.transform = ("translateX(0px)")
+        window.setTimeout(function(){
+            somb.classList=("sombra")
+        },200)
     }
     i++
     if (i >= 2) {
-        move.style.transform = ("translateX(0px)")
+        move.style.transform = ("translateX(-300px)")
+        hid.style.overflow =""
+        somb.classList.remove("sombra")
         i = 0
     }
 })
