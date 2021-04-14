6b4ff9232ce800a4eccb04a794f6dc5683ac0823
Blacklist14 <luis.arigato69@hotmail.com>
2021-04-14 20:02:03 -0300

    Update 0.3

--- javascript/Menu.js
+++ javascript/Menu.js
@@ -2,15 +2,41 @@ i = 0
 //ANIMAÇÃO-MENU\\
 var menu = document.querySelector(".hamburgue").addEventListener("click", () => {
     let move = document.querySelector(".menu-content")
+    let video = document.querySelector(".content-video")
     let somb = document.querySelector("#sombra")
     let hid = document.querySelector(".content-video")
+    let list = document.querySelector(".lista-main ul")
+    let v = document.querySelector(".video-gif")
+    let v1 = document.querySelector(".video-gif1")
+    let v2 = document.querySelector(".video-gif2")
+    let v3 = document.querySelector(".video-gif3")
+    let v4 = document.querySelector(".video-gif4")
+    let v5 = document.querySelector(".video-gif5")
+    let v6 = document.querySelector(".video-gif6")
+    let v7 = document.querySelector(".video-gif7")
+    let v8 = document.querySelector(".video-gif8")
+    let v9 = document.querySelector(".video-gif9")
+    let v10 = document.querySelector(".video-gif10")
+    let v11 = document.querySelector(".video-gif11")
     //MOSTRA-O-MENU\\
     if (i <= 0) {
+        
         hid.style.overflow = "hidden"
         move.style.transform = ("translateX(0px)")
+        video.style.width = "81vw"
         window.setTimeout(function () {
             somb.classList = ("sombra")
         }, 200)
+        list.style.width= "82.5vw"
+        v.style.width = '250px'
+        v1.style.width = '250px'
+        v2.style.width = '250px'
+        v3.style.width = '250px'
+        v4.style.width = '250px'
+        v5.style.width = '250px'
+        v6.style.width = '250px'
+        v7.style.width = '250px'
+        v8.style.width = '250px'
     }
     //END MOSTRA-O-MENU\\
 
@@ -19,6 +45,17 @@ var menu = document.querySelector(".hamburgue").addEventListener("click", () => 
         move.style.transform = ("translateX(-300px)")
         hid.style.overflow = ""
         somb.classList.remove("sombra")
+        video.style.width = "95vw"
+        list.style.width= "95vw"
+        v.style.width = '300px'
+        v1.style.width = '300px'
+        v2.style.width = '300px'
+        v3.style.width = '300px'
+        v4.style.width = '300px'
+        v5.style.width = '300px'
+        v6.style.width = '300px'
+        v7.style.width = '300px'
+        v8.style.width = '300px'
         i = 0
     })
     // END REMOVE-MEU-SE-CLICA-NA-SOMBRA
@@ -30,6 +67,17 @@ var menu = document.querySelector(".hamburgue").addEventListener("click", () => 
         move.style.transform = ("translateX(-300px)")
         hid.style.overflow = ""
         somb.classList.remove("sombra")
+        video.style.width = "95vw"
+        list.style.width= "95vw"
+        v.style.width = '300px'
+        v1.style.width = '300px'
+        v2.style.width = '300px'
+        v3.style.width = '300px'
+        v4.style.width = '300px'
+        v5.style.width = '300px'
+        v6.style.width = '300px'
+        v7.style.width = '300px'
+        v8.style.width = '300px'
         i = 0
     }
     //END-REMOVE-MENU\\
