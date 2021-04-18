ff1fd1b7f90ca24450a39c1cf95b9ef6c753166a
Blacklist14 <luis.arigato69@hotmail.com>
2021-04-17 22:16:08 -0300

    Update 0.6

--- javascript/Menu.js
+++ javascript/Menu.js
@@ -1,58 +1,29 @@
 i = 0
 //ANIMAÇÃO-MENU\\
-var menu = document.querySelector(".hamburgue").addEventListener("click", () => {
+
+document.querySelector(".hamburgue").addEventListener("click", () => {
     let move = document.querySelector(".menu-content")
-    let video = document.querySelector(".content-video")
     let somb = document.querySelector("#sombra")
     let hid = document.querySelector(".content-video")
     let list = document.querySelector(".lista-main ul")
-    let v = document.querySelector(".video-gif")
-    let v1 = document.querySelector(".video-gif1")
-    let v2 = document.querySelector(".video-gif2")
-    let v3 = document.querySelector(".video-gif3")
-    let v4 = document.querySelector(".video-gif4")
-    let v5 = document.querySelector(".video-gif5")
-    let v6 = document.querySelector(".video-gif6")
-    let v7 = document.querySelector(".video-gif7")
-    let v8 = document.querySelector(".video-gif8")
     //MOSTRA-O-MENU\\
-    p=0
+    p = 0
     if (i <= 0) {
         hid.style.overflow = "hidden"
         move.style.transform = ("translateX(0px)")
-        video.style.width = "81vw"
         window.setTimeout(function () {
             somb.classList = ("sombra")
         }, 200)
         list.style.transform = "translateX(150px)"
-        v.style.width = '250px'
-        v1.style.width = '250px'
-        v2.style.width = '250px'
-        v3.style.width = '250px'
-        v4.style.width = '250px'
-        v5.style.width = '250px'
-        v6.style.width = '250px'
-        v7.style.width = '250px'
-        v8.style.width = '250px'
     }
     //END MOSTRA-O-MENU\\
 
     //REMOVE-MENU-SE-CLICA-NA-SOMBRA\\
-    let remS = document.querySelector("#sombra").addEventListener("click", () => {
+    document.querySelector("#sombra").addEventListener("click", () => {
         move.style.transform = ("translateX(-300px)")
         hid.style.overflow = ""
         somb.classList.remove("sombra")
-        video.style.width = "95vw"
-        list.style.transform = "translateX(90px)"
-        v.style.width = '300px'
-        v1.style.width = '300px'
-        v2.style.width = '300px'
-        v3.style.width = '300px'
-        v4.style.width = '300px'
-        v5.style.width = '300px'
-        v6.style.width = '300px'
-        v7.style.width = '300px'
-        v8.style.width = '300px'
+        list.style.transform = "translateX(0px)"
         i = 0
     })
     // END REMOVE-MEU-SE-CLICA-NA-SOMBRA
@@ -64,24 +35,14 @@ var menu = document.querySelector(".hamburgue").addEventListener("click", () => 
         move.style.transform = ("translateX(-300px)")
         hid.style.overflow = ""
         somb.classList.remove("sombra")
-        video.style.width = "95vw"
-        list.style.transform = "translateX(90px)"
-        v.style.width = '300px'
-        v1.style.width = '300px'
-        v2.style.width = '300px'
-        v3.style.width = '300px'
-        v4.style.width = '300px'
-        v5.style.width = '300px'
-        v6.style.width = '300px'
-        v7.style.width = '300px'
-        v8.style.width = '300px'
+        list.style.transform = "translateX(0px)"
         i = 0
     }
     //END-REMOVE-MENU\\
 })
 //END ANIMAÇÃO-MENU\\
 n = 0
-var op1 = document.querySelector(".option1").addEventListener("click", () => {
+document.querySelector(".option1").addEventListener("click", () => {
     var op0 = document.querySelector(".option1")
     if (n <= 0) {
         op0.style.backgroundColor = ("rgba(255, 255, 255, 0.171)")
@@ -96,35 +57,36 @@ var op1 = document.querySelector(".option1").addEventListener("click", () => {
 //MOVE AS TAGS
 soma = 100
 var b1 = document.querySelector(".bottonE")
-var mov1 = document.querySelector(".bottonE").addEventListener("click",()=>{
+document.querySelector(".bottonE").addEventListener("click", () => {
     let list = document.querySelector(".lista-main ul")
-    if("click"){
-    list.style.transform= `translateX(${soma}px)`
-    var s = soma +=70
-    b2.style.display="flex"
+    if ("click") {
+        list.style.transform = `translateX(${soma}px)`
+        var s = soma += 70
+        b2.style.display = "flex"
     }
-    if(soma >= 70){
-        b1.style.display= "none"
+    if (soma >= 70) {
+        b1.style.display = "none"
         som = 0
     }
 
 })
 var b2 = document.querySelector(".bottonD")
 som = 100
-var mov2 = document.querySelector(".bottonD").addEventListener("click",()=>{
+document.querySelector(".bottonD").addEventListener("click", () => {
     let list = document.querySelector(".lista-main ul")
-    if("click"){
-    list.style.transform= `translateX(-${som}px)`
-    som +=70
-    console.log(som)
-    b1.style.display="flex"
+    if ("click") {
+        list.style.transform = `translateX(-${som}px)`
+        som += 70
+        console.log(som)
+        b1.style.display = "flex"
     }
-    if(som >= 560){
-        b2.style.display= "none"
+    if (som >= 560) {
+        b2.style.display = "none"
         soma = -470
     }
 
 })
+
 function showBtn() {
     var calc = document.querySelector('body > main > section.lista-main').offsetWidth - document.querySelector('body > main > section.lista-main > ul').offsetWidth + (document.querySelector('body > main > section.lista-main > ul').offsetLeft * -1)
     console.log(calc)
@@ -134,7 +96,7 @@ function showBtn() {
         document.querySelector('body > main > div.bottonD').style.display = 'none';
     }
 
-    if(calc > -526) {
+    if (calc > -526) {
         document.querySelector('body > main > div.bottonE').style.display = 'flex';
         // document.querySelector('body > main > div.bottonE').style.left = '-80px';
     } else {
